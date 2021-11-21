3d24f22e090bd6d0d095e4915659c8d9d5082812
katietang1 <78448486+katietang1@users.noreply.github.com>
2021-11-21 11:59:41 -0700

    Created xCode project

---
+++ WeatherApp_TangYu/Shared/WeatherApp_TangYuApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_TangYuApp.swift
+//  Shared
+//
+//  Created by Katie Tang on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_TangYuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
