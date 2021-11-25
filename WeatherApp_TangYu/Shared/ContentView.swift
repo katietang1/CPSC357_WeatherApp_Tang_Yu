ef56b57e09ca8137bb339c023489a725385c8f18
katietang1 <78448486+katietang1@users.noreply.github.com>
2021-11-24 17:17:28 -0700

    Added tabs, need to figure out how to seperate them so we can live view all 3 tabs

--- WeatherApp_TangYu/Shared/ContentView.swift
+++ WeatherApp_TangYu/Shared/ContentView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 1
     var body: some View {
-        Text("Hello, is this working")
+        TabView(selection: $selection){
+            
+            Text("weather stuff here")
+                .tabItem {
+                    Label("Forecast", systemImage: "cloud")
+                } .tag(1)
+            
+            Text("Outfit Suggestions here")
+                .tabItem {
+                    Label("OOTD", systemImage: "sparkles")
+                } .tag(2)
+            
+            Text("List of clothes")
+                .tabItem {
+                    Label("My Closet", systemImage: "face.smiling")
+                    //tried to use tshirt but didnt work
+                } .tag(3)
+            
+        }.font(.largeTitle)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
