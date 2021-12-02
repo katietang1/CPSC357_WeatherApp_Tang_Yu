6c359afc9871b8db4bd89a54124222ff39482e28
katietang1 <78448486+katietang1@users.noreply.github.com>
2021-12-02 13:52:48 -0800

    SwiftUI file added

---
+++ WeatherApp_TangYu/Shared/OOTD.swift
@@ -0,0 +1,20 @@
+//
+//  OOTD.swift
+//  WeatherApp_TangYu
+//
+//  Created by Katie Tang on 12/2/21.
+//
+
+import SwiftUI
+
+struct OOTD: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OOTD_Previews: PreviewProvider {
+    static var previews: some View {
+        OOTD()
+    }
+}
