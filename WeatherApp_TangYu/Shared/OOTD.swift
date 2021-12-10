935c4e9a453b3b5c340c4abbad7bc5c010b92114
Minna Yu <minyu@chapman.edu>
2021-12-09 22:23:51 -0800

    Minor edits

--- WeatherApp_TangYu/Shared/OOTD.swift
+++ WeatherApp_TangYu/Shared/OOTD.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct OOTD: View {
+   
     var body: some View {
-        Text("YASSS")
+        Text("OOTD")
         
     }
+
 }
+
+
 
 struct OOTD_Previews: PreviewProvider {
     static var previews: some View {
